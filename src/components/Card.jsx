a79652c0f7fe47783d13f0380c4784257671da3f
DaneshwarVerma <techydanesh@gmail.com>
2024-08-14 21:47:52 +0530

    make project card rendering dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-function Card(Props){
+function Card(props){
     return (<div className="card  p-3 rounded-md bg-slate-700 text-white ">
-        <p className="Title font-bold">{Props.name}</p>
+        <p className="Title font-bold">{props.name}</p>
         <div className="image my-2 border-slate-500 border-2 rounded-md">
-        <img className="rounded-md" src="src\assets\bloghost.png" alt="" /></div>
+        <img className="rounded-md" src={props.img} alt="" /></div>
         <hr />
-        <p className="Description text-sm">{Props.des}</p>
+        <p className="Description text-sm">{props.des}</p>
     </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,23 @@
 import Card from "./Card"
 function Projects(){
+    const projects = [
+        {   
+            name: "Online Music Platform",
+            img: `src/assets/Projects/Spotesh.jpg`,
+            des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae non voluptatibus iusto rem optio facilis cupiditate consequuntur, commodi excepturi ut laudantium iure quasi ea libero explicabo aliquid deserunt. Explicabo aliquid animi iure eum."
+        },
+        {
+            name: "Blog Website",
+            img: `src/assets/Projects/bloghost`,
+            des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa beatae non voluptatibus iusto rem optio facilis cupiditate consequuntur, commodi excepturi ut laudantium iure quasi ea libero explicabo aliquid deserunt. Explicabo aliquid animi iure eum."
+        }
+    ]
     return (
        <div className="Projects bg-white mx-4 my-2">
-        <Card name={'The BlogHost Site'}  des="A Personal Blog  Site to share own knowledge to internet surfers. that site is Made by using Node, express.js at server side. the client side is created using the Bootsrap components & utility classes functionalities site has the crud functionalities to manage managing Blog post data it "/>
-       </div>
+        {projects.map((e, index)=>
+         <Card key={index} name={e.name} des={e.des} img={e.img}/>
+        )}
+      </div>
     )
 }
 
