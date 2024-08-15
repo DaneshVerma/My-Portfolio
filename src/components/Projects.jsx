7132a573b2fa5ec2b375889d6adcd34eb5b3f337
DaneshwarVerma <techydanesh@gmail.com>
2024-08-15 13:44:50 +0530

    made project card carousal done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,9 @@
 import Card from "./Card";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css/autoplay';
+import 'swiper/css';
 function Projects() {
   const projects = [
     {
@@ -21,20 +26,38 @@ function Projects() {
     },
   ];
   return (
-    <div id="projects" className="projects rounded-sm" >
-      <div  className="intro h-10 rounded-md mx-4 mt-2 p-1 bg-white border-2 border-slate-500 text-center font-bold">
-      Projects i did From Scratch 
-        </div>
-      <div className="h-[28rem] flex flex-col items-center rounded-md overflow-x-auto bg-white mx-4 my-2">
-        {projects.map((e, index) => (
-          <Card
-            key={index}
-            name={e.name}
-            des={e.des}
-            img={e.img}
-            link={e.link}
-          />
-        ))}
+    <div id="projects" className="projects rounded-sm">
+      <div className="intro h-10 rounded-md mx-4 mt-2 p-1 bg-white border-2 border-slate-500 text-center font-bold">
+        Projects i did From Scratch
+      </div>
+      {/* <div className="h-[28rem] flex flex-col items-center rounded-md overflow-x-auto bg-white mx-4 my-2"> */}
+      <div className=" rounded-md bg-white mx-4 my-2">
+        <Swiper
+           spaceBetween={30}
+           centeredSlides={true}
+           modules={[Autoplay]}
+           autoplay={{
+             delay: 2000,
+             pauseOnInteraction: false
+           }}
+           pagination={{
+             clickable: true,
+           }}
+           navigation={true}
+          //  modules={[Autoplay, Pagination, Navigation]}
+           className="mySwiper"
+        >
+          {projects.map((e, index) => (
+            <SwiperSlide  key={index}>
+              <Card
+                name={e.name}
+                des={e.des}
+                img={e.img}
+                link={e.link}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
